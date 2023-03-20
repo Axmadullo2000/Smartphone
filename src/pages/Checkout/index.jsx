1df23a9a1230fa2e3c8972a7b7a4e958622d6193
abdulvakhid <buriyevabdulvohid055@gmail.com>
2023-03-20 21:47:57 +0500

    add

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -1,35 +1,33 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import Footer from '../../components/Footer'
-import Header from '../../components/Header'
-import {useTranslation} from "react-i18next"
-import './Checkout.scss'
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import { useTranslation } from "react-i18next";
+import "./Checkout.scss";
 
 export const Checkout = () => {
-	const {t} = useTranslation();
+	const { t } = useTranslation();
 	return (
 		<>
 			<Header />
-
-			<div className='checkout_container ml-7'>
-				<ul className='flex  mt-5'>
-					<li className='checkout_direction'>
-						<Link to='/'>{t("cardDetail.main")}</Link>
+			<div className="checkout_container ml-7">
+				<ul className="flex  mt-5">
+					<li className="checkout_direction">
+						<Link to="/">{t("cardDetail.main")}</Link>
 					</li>
-					<li className='checkout_direction mx-4'>
-						<Link to='/customer/cart/'>{t("basket.cart")}</Link>
+					<li className="checkout_direction mx-4">
+						<Link to="/customer/cart/">{t("basket.cart")}</Link>
 					</li>
-					<li className='checkout_direction'>{t("checkout.order")}</li>
+					<li className="checkout_direction">{t("checkout.order")}</li>
 				</ul>
 
-				<h2 className='mt-5 get_offer_checkout'>{t("checkout.order")}</h2>
+				<h2 className="mt-5 get_offer_checkout">{t("checkout.order")}</h2>
 
 				<div>
 					<h3>{t("checkout.orderinfo")}</h3>
 				</div>
 			</div>
-
 			<Footer />
 		</>
-	)
-}
+	);
+};
